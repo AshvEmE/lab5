10bc1af7d142d096b2d6d37788b2f05df72c8af5
AshvEmE <ashveme@gmail.com>
2024-03-07 23:48:13 +0200

    1.2

--- script.js
+++ script.js
@@ -16,7 +16,54 @@ function checkEvenNumber(num) {
   
 }
 
+function getMonthName(monthNumber) {
+	if (typeof monthNumber !== 'number' || monthNumber < 1 || monthNumber > 12) {
+		return 'Помилка'
+	}
 
+	let monthName
+	switch (monthNumber) {
+		case 1:
+			monthName = 'Січень'
+			break
+		case 2:
+			monthName = 'Лютий'
+			break
+		case 3:
+			monthName = 'Березень'
+			break
+		case 4:
+			monthName = 'Квітень'
+			break
+		case 5:
+			monthName = 'Травень'
+			break
+		case 6:
+			monthName = 'Червень'
+			break
+		case 7:
+			monthName = 'Липень'
+			break
+		case 8:
+			monthName = 'Серпень'
+			break
+		case 9:
+			monthName = 'Вересень'
+			break
+		case 10:
+			monthName = 'Жовтень'
+			break
+		case 11:
+			monthName = 'Листопад'
+			break
+		case 12:
+			monthName = 'Грудень'
+			break
+		default:
+			monthName = 'Невідомий місяць'
+	}
+	return monthName
+}
 
 
 // Перевірка на паліндром стрілковою функцією(string)
@@ -34,7 +81,22 @@ let checkPalindrome = (str) => {
 };
 
 
-// Функція що перевіряє чи об'єкт порожній(object)
+function checkNum(num) {
+	if (typeof num !== 'number') {
+		return 'Помилка'
+	}
+
+	if (num > 0) {
+		return 'Додатнє число'
+	} else if (num < 0) {
+		return "Від'ємне число"
+	} else {
+		return 'Нуль'
+	}
+}
+
+
+//Перевірка чи об'єкт порожній
 const checkEmptyObject = function (obj) {
 	if (typeof obj !== 'object') {
 		return "Помилка"
@@ -46,6 +108,7 @@ const checkEmptyObject = function (obj) {
 		return false
 	}
 }
+
 
 
 class Animal {
